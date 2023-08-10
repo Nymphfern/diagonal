f98934645daf28d4b2c30bc97b03096b22feda62
79511 <79511@LAPTOP-EJU4OS3A>
2023-08-10 22:58:34 +0700

    Добавьте файлы проекта.

---
+++ tren.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    const int rows = 10;
+    const int cols = 10;
+
+    int mas[rows][cols];
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+
+            if (i == j) {
+
+                mas[i][j] = 1;
+            }
+            else {
+
+                mas[i][j] = 0;
+            }
+            cout << mas[i][j]<< "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+           
+
